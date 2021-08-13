d3659f4bf8ad922339512338926e2c1de369aea9
Grzesiek <grzmacko@gmail.com>
2021-08-13 20:17:24 +0200

    Reset

--- src/index.js
+++ src/index.js
@@ -16,12 +16,16 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 */
+
+
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
 
 ReactDOM.render(
   <BrowserRouter>
